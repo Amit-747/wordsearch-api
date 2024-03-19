8bc14c23f5d8b5cfd11c0a87ea998cdbfb8f53fd
soy-amit <92292345+soy-amit@users.noreply.github.com>
2024-03-18 21:43:30 -0700

    WordSearchAPI Implementation

---
+++ src/test/java/io/amitksri/wordsearchapi/WordsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.amitksri.wordsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
